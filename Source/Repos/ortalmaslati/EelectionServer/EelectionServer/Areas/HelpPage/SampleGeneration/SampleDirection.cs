5da0f4f6fb7ce6c1e24dd02da1f87c447ab32ba4
Ortal Maslati <ortalmaslati2@gmail.com>
2019-11-03 22:42:34 +0200

    First commit

---
+++ Source/Repos/ortalmaslati/EelectionServer/EelectionServer/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace EelectionServer.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ Source/Repos/ortalmaslati/EelectionServer/EelectionServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EelectionServer.WebApiApplication" Language="C#" %>
